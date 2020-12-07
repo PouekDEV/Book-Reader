51595f893a8ac5287f19ee3979e46599be93fe7c
Jakub Starostecki <64737924+PouekDEV@users.noreply.github.com>
2020-12-07 13:25:50 +0100

    v.0.5

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 from time import sleep
 width = 1280
 height = 720
-yoftext = -1.5
+yoftext = float(-1.5)
 title = "Book Reader"
 text = "Put here a file with text or click enter"
 thing = ""
@@ -30,39 +30,44 @@ new_label = pyglet.text.Label(text,
                           font_size = 10, 
                           x = 25, y = 25) 
 
-label2 = pyglet.text.Label(etc, 
-                          font_name ='Josefin Sans', 
-                          font_size = 18, 
-                          x = window.width//2, y = window.height//1.1, 
-                          anchor_x ='center', anchor_y ='center') 
 batch = pyglet.graphics.Batch()
 
 document = pyglet.text.document.FormattedDocument()
 document.insert_text(0, thing, attributes=dict(font_name='Arial', font_size=20, color=(255, 255, 255, 255)))
-layout = pyglet.text.layout.IncrementalTextLayout(document, width, height, multiline=True, batch=batch)
+layout = pyglet.text.layout.IncrementalTextLayout(document, width*2, height*2, multiline=True, batch=batch)
 
-@window.event
-def seting():
+def add():
     global yoftext
-def on_key_press(symbol, modifiers):
-        if symbol == key.S:
-            yoftext -= 1
-            print("dwon")
-        if symbol == key.W:
-            yoftext += 1
+    yoftext -= 1
+    print(yoftext)
+def substract():
+    global yoftext
+    yoftext += 1
+    print(yoftext)
+@window.event
+def on_key_press(symbol, modifier):
+        if symbol == pyglet.window.key.S:
+            print("down")
+            add()
+        if symbol == pyglet.window.key.W:
             print("up")
+            substract()
 @window.event 
-def on_draw():      
-    window.clear() 
-#    label.draw() 
+def on_draw():     
+    window.clear()  
+    label2 = pyglet.text.Label(etc, 
+                          font_name ='Josefin Sans', 
+                          font_size = 18, 
+                          x = window.width//2, y = (650 + yoftext * 50), 
+                          anchor_x ='center', anchor_y ='center') 
     label2.draw()
     layout.width = 0.9*width
     layout.height = 0.9*height
-    layout.x = (width - layout.width)*0.5
+    layout.x = (width - layout.width)*1.5
     layout.y = (height - layout.height)*yoftext
     batch.draw()
-    end.blit(750,580)
-    book.blit(400,580)
+    end.blit(750,580 + yoftext * 50)
+    book.blit(400,580 + yoftext * 50)
 img = image = pyglet.resource.image("assets/icon.png")
 window.set_icon(img)
 pyglet.app.run() 
